2b92a36597f41aaabb0f8a1ddd621e39e5f03b00
Kristian Bossev <bossev91@gmail.com>
2021-03-05 12:02:38 +0200

    TaskOOP

---
+++ Inheritance/Restaurant/Beverage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Beverage : Product
+    {
+        public Beverage(string name, decimal price, double milliliters) : base(name, price)
+        {
+            Mililliters = milliliters;
+        }
+
+        public double Mililliters { get; set; }
+    }
+}
---
+++ Inheritance/Restaurant/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Cake cake = new Cake("Space Cake");
+
+            System.Console.WriteLine($"{cake.Name} - {cake.Grams} - {cake.Calories} - {cake.Price}");
+        }
+    }
+}

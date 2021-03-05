a245f7e581e4c8488209d8b58c61ee7519beb4b7
Kristian Bossev <bossev91@gmail.com>
2021-03-05 11:18:48 +0200

    tasks

---
+++ NeedForSpeed/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RaceMotorcycle raceMotor = new RaceMotorcycle(200, 100);
+
+            System.Console.WriteLine(raceMotor.FuelConsumption);
+
+            Car car = new Car(200, 100);
+
+            System.Console.WriteLine(car.FuelConsumption);
+
+            Motorcycle motorcycle = new Motorcycle(200, 100);
+
+            System.Console.WriteLine($"MotorCycle consuption = {motorcycle.FuelConsumption}");
+
+            SportCar sportCar = new SportCar(200, 100);
+
+            System.Console.WriteLine(sportCar.FuelConsumption);
+        }
+    }
+}
